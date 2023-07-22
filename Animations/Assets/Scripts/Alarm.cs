957a3650aa593c40ab2c3866a47741aec463b232
Chernikova <104696269+EkaterinaChernikova@users.noreply.github.com>
2023-07-22 21:07:57 +0300

    Thief dz№5

---
+++ Animations/Assets/Scripts/Alarm.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Alarm : MonoBehaviour
+{
+    private int alarmVolumeChange = -1;
+    [SerializeField] private float volumeChangeSpeed = 0.5f; 
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Thief")
+        {
+            Debug.Log("ALARM");
+            alarmVolumeChange *= -1;
+        }
+    }
+
+    void Update()
+    {
+        GetComponent<AudioSource>().volume += alarmVolumeChange * Time.deltaTime * volumeChangeSpeed;
+    }
+}
---
+++ Animations/Assets/Scripts/Movement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+    [SerializeField] private float _rotationSpeed;
+    private float _animationSwitch = 0.0f;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.S) 
+            || Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.D))
+        {
+            _animationSwitch = 0.05f;
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.forward * _speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.back * _speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.down * _rotationSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(Vector3.up * _rotationSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.W) == false && Input.GetKey(KeyCode.S) == false
+            && Input.GetKey(KeyCode.A) == false && Input.GetKey(KeyCode.D) == false)
+        {
+            _animationSwitch = 0.0f;
+        }
+
+        GetComponent<Animator>().SetFloat("Speed", _animationSwitch);
+    }
+}
